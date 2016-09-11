5bfac5bd5c1192b4db87a652cfd715815ced2819
jamescelliers <jamescelliers@hotmail.co.uk>
2016-09-11 16:36:40 +0100

    ParseCloneName(). Removes bracketed suffix from GameObject name.

---
+++ ExtensionMethods.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class ExtensionMethods
+{
+
+    /// <summary>
+    /// Removes Bracketed Suffix By finding first "(" and removes chars from there
+    /// (Useful for using name for GameObject identification, just dont use (), instead use [] or <>)
+    /// </summary>
+    /// <param name="objectToRename">GameObject to change name of</param>
+    public static void ParseCloneName(this GameObject objectToRename)
+    {
+        string s = objectToRename.name;
+        int startParse = 0;
+        char open = '(';
+
+        for (int i = 0; i < objectToRename.name.Length; i++)
+        {
+            if (objectToRename.name[i].Equals(open))
+            {
+                startParse = i;
+            }
+        }
+        int count = objectToRename.name.Length - startParse;
+        s = s.Remove(startParse, count);
+        objectToRename.name = s;
+    }
+
+}
